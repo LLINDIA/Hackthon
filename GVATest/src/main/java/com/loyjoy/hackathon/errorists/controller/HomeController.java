e6cedaa97d3e5beab5bab1c4acf41fce24c06c78
AnkitA <AnkitA@Ankit-Laptop.LOYLOGIC.COM>
2018-01-06 11:54:46 +0530

    Spring MVC home controller

--- GVATest/src/main/java/com/loyjoy/hackathon/errorists/controller/HomeController.java
+++ GVATest/src/main/java/com/loyjoy/hackathon/errorists/controller/HomeController.java
@@ -3,10 +3,18 @@
  */
 package com.loyjoy.hackathon.errorists.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author AnkitA
  *
  */
+@Controller
 public class HomeController {
 
+	@RequestMapping("/voodoo")
+	public String fetchResults() {
+		return "/jsp/upload.jsp";
+	}
 }
