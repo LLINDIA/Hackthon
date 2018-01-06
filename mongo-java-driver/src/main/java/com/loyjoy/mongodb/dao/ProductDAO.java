94f1bd0ad52d36016bff554d6dbcd724a478a892
irfanpara <irfan.paraniya@loylogic.com>
2018-01-06 15:09:55 +0530

    Mongo connector

---
+++ mongo-java-driver/src/main/java/com/loyjoy/mongodb/dao/ProductDAO.java
@@ -0,0 +1,32 @@
+package com.loyjoy.mongodb.dao;
+
+public class ProductDAO {
+	private String id;
+	private String keyword;
+	private String description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		description = description;
+	}
+
+}
